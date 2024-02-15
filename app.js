bca132ffa9eaff27343ab2fdd5a890f6e50091a8
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-02-15 13:08:19 +0700

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const morgan = require('./middlewares/morgan');
 const helmet = require('helmet');
 
+// middleware
 app.use(morgan());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
@@ -14,8 +15,7 @@ const routes = require('./routes');
 app.use('/', routes);
 
 const port = process.env.PORT || 3560;
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log('\x1b[36m', `Server is running on port ${port}`, '\x1b[0m');
+  console.log('\x1b[36m', `Server is running on port ${port}`);
 });
