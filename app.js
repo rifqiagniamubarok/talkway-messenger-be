21c591a57fe9dbd9f373a42ed837674e5c4a8f1e
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-02-15 10:42:42 +0700

    Feat: eslint

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3560;
 
 app.use(express.urlencoded({ extended: false }));
+// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routing
 const routes = require('./routes');
 app.use('/', routes);
 
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3560;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
