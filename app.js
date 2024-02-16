eac0c69a333e27632026a592bcd075920f383619
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-02-16 07:15:01 +0700

    initial commit

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const path = require('path');
 const app = express();
 const morgan = require('./middlewares/morgan');
 const helmet = require('helmet');
+const { sequelize } = require('./models');
 
 // middleware
 app.use(morgan());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Test db connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // routing
 const routes = require('./routes');
@@ -17,5 +28,5 @@ app.use('/', routes);
 const port = process.env.PORT || 3560;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log('\x1b[36m', `Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
